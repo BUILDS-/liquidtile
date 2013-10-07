3b6a9873a547c66f571f9f50ec7f393403a15d2b
Christopher Woodall <chris.j.woodall@gmail.com>
2013-10-06 21:44:47 -0400

    Changed test0.py so it takes the port as a command line argument

--- software/test0.py
+++ software/test0.py
@@ -1,7 +1,27 @@
+#!python
+##
+# test0.py  
+# Authors: Christopher J. Woodall <chris.j.woodall at gmail.com>
+# 
+# Test code which ramps up the r g and b values down
+# each column.
+##
+
 import pyFireTile
 from time import sleep
+import sys
 
-t = led_tile.LEDTile("/dev/tty.usbserial-A600aflQ")
+if len(sys.argv) == 1:
+    print "useage: test0.py <serial-port>"
+    sys.exit(0)
+elif len(sys.argv) == 2:
+    port = sys.argv[1]
+else:
+    print "useage: test0.py <serial-port>"
+    sys.exit(0)
+
+print port
+t = pyFireTile.FireTile3x3(port)
 t.open()
 t.clear()
 
@@ -15,7 +35,7 @@ try:
                     t.setPixel(i, [0, j, 0])
                 else:   
                     t.setPixel(i, [0, 0, j])
-            t.ser.write(':u\n')
+            t.update()
             sleep(.1)
         for j in reversed(range(255)[0::16]):
             for i in range(9):
@@ -25,7 +45,7 @@ try:
                     t.setPixel(i, [0, j, 0])
                 else:   
                     t.setPixel(i, [0, 0, j])
-            t.ser.write(':u\n')
+            t.update()
             sleep(.1)
 except:
     print "CLOSING!!!"
